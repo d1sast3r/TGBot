420e15c59f4e1cd2a3bf9366bec68dfc27e66cc7
d1sast3r <sh.vladimir.m@gmail.com>
2022-07-29 10:54:03 +0600

    added all files with TG Bot project

---
+++ StudyBot/repeater.py
@@ -0,0 +1,13 @@
+import requests
+
+API_LINK = "https://api.telegram.org/bot5500847779:AAGvLeoWW4eJccjF767IU8tR1tXpM0WVYyQ"
+
+UPDATES = requests.get(API_LINK + "/getUpdates?offset=-1").json()
+
+print(UPDATES)
+
+message = UPDATES['result'][0]['message']
+chat_id = message['from']['id']
+text = message['text']
+
+sent_message = requests.get(API_LINK + f'/sendMessage?chat_id={chat_id}&text=Привет, ты написал {text}')
